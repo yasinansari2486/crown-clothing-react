26837cb667a7471f3bab48a148b787fa0b1ab2cf
yasinansari2486 <yasinansari2486@gmail.com>
2021-09-17 12:02:43 +0530

    added header component & links to home, shop and contact

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Switch,Route } from 'react-router-dom';
+
 import './App.css';
+
+import Header from './components/header/header.component';
 import HomePage from "./pages/homepage/homepage.component"
 import ShopPage from './pages/shop/shop.component';
 
@@ -8,6 +11,7 @@ import ShopPage from './pages/shop/shop.component';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -9,8 +9,15 @@ const CollectionItem = ({id,name,price,imageUrl}) => (
             backgroundImage: `url(${imageUrl})`
         }} />
         <div className="collection-footer">
-            <span className="name">{name}</span>
-            <span className="price">&#36; {price}</span>
+            <div className="collection-info">
+                <span className="name">{name}</span>
+                <span className="price">&#36; {price}</span>
+            </div>
+            <div className="collection-btn">
+                <i class="fas fa-shopping-cart fa-1x"></i>
+                <button>Buy Now</button>
+            </div>
+           
         </div>
         
     </div>
