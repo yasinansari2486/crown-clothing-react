e807f1a967c7bd34667fd8c2e6360d67db70ef30
yasinansari2486 <yasinansari2486@gmail.com>
2021-09-20 15:51:26 +0530

    added some changes for gh-pages

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -15,7 +15,7 @@ const Header = () => (
             <Link className="option" to="/shop">
                 SHOP
             </Link>
-            <Link className="option" to="/shop">
+            <Link className="option" to="/signin">
                 CONTACT
             </Link>
         </div>
