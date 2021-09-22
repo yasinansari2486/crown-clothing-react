296e0e24b717cf207ea1150126e08c4f4b37f2cf
yasinansari2486 <yasinansari2486@gmail.com>
2021-09-22 18:17:39 +0530

    included ability to store authenticated user into firebase database

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -41,13 +41,13 @@ class SignIn extends React.Component{
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
                 <form onSubmit={this.handleSubmit}>
-                    <FormInput type="email" name="email" value={this.state.email}  handleChange={this.handleChange} label='email' required />
-                    <FormInput type="password" name="password" value={this.state.password} handleChange={this.handleChange} label='password' required />
+                    <FormInput type="email" name="email" value={this.state.email}  handleChange={this.handleChange} label='Email' required />
+                    <FormInput type="password" name="password" value={this.state.password} handleChange={this.handleChange} label='Password' required />
                     <div className="buttons">
                         <CustomButton type="submit">Sign In</CustomButton>
                         <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
                             {''}
-                            Sign in with google{''}
+                            Sign in with Google{''}
                         </CustomButton>
                     </div>
                     
